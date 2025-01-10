c49b35e387f05deb72d770937c549691a1215fae
andrewtarzia <andrew.tarzia@gmail.com>
2025-01-10 15:01:10 +0100

    Refactor.

--- src/shc/study_2_case_study_1.py
+++ src/shc/study_2_case_study_1.py
@@ -196,65 +196,6 @@ def analyse_ligand_pair(  # noqa: PLR0913
     )
 
 
-def extract_torsions(
-    molecule: stk.Molecule,
-    smarts: str,
-    expected_num_atoms: int,
-    scanned_ids: tuple[int, int, int, int],
-    expected_num_torsions: int,
-) -> tuple[float, float]:
-    """Extract two torsions from a molecule."""
-    rdkit_molecule = molecule.to_rdkit_mol()
-    matches = rdkit_molecule.GetSubstructMatches(
-        query=rdkit.MolFromSmarts(smarts),
-    )
-
-    torsions = []
-    for match in matches:
-        if len(match) != expected_num_atoms:
-            msg = f"{len(match)} not as expected ({expected_num_atoms})"
-            raise RuntimeError(msg)
-        torsions.append(
-            abs(
-                rdMolTransforms.GetDihedralDeg(
-                    rdkit_molecule.GetConformer(0),
-                    match[scanned_ids[0]],
-                    match[scanned_ids[1]],
-                    match[scanned_ids[2]],
-                    match[scanned_ids[3]],
-                )
-            )
-        )
-    if len(torsions) != expected_num_torsions:
-        msg = f"{len(torsions)} found, not {expected_num_torsions}!"
-        raise RuntimeError(msg)
-    return tuple(torsions)
-
-
-def get_amide_torsions(molecule: stk.Molecule) -> tuple[float, float]:
-    """Get the centre alkene torsion from COOH."""
-    smarts = "[#6X3H0]~[#6X3H1]~[#6X3H0]~[#6X3H0](=[#8])~[#7]"
-    expected_num_atoms = 6
-    scanned_ids = (1, 2, 3, 5)
-
-    return extract_torsions(
-        molecule=molecule,
-        smarts=smarts,
-        expected_num_atoms=expected_num_atoms,
-        scanned_ids=scanned_ids,
-        expected_num_torsions=1,
-    )
-
-
-def get_num_alkynes(rdkit_mol: rdkit.Mol) -> int:
-    """Get the number of alkynes in a molecule."""
-    smarts = "[#6]#[#6]"
-    matches = rdkit_mol.GetSubstructMatches(
-        query=rdkit.MolFromSmarts(smarts),
-    )
-    return len(matches)
-
-
 def plot_ligand(
     ligand_name: str,
     ligand_db: atomlite.Database,
