1ed6e99bec0dbe3e0be24c51127e8b36efaa9838
andrewtarzia <andrew.tarzia@gmail.com>
2025-01-10 14:59:55 +0100

    Use env variables in plot.

--- src/shc/visualise_scoring_function.py
+++ src/shc/visualise_scoring_function.py
@@ -5,38 +5,39 @@ import pathlib
 import matplotlib.pyplot as plt
 import numpy as np
 
+from shc.definitions import EnvVariables, res_str
+
 
 def scoring_function(x: np.ndarray, target: float, beta: float) -> np.ndarray:
     """Define ff."""
     return 1 / (1 + np.exp(beta * (x - target)))
-    return 20 * np.exp(-1 * x)
-
-    return 10 * np.exp(-1 * x)
-    return 20 * np.exp(-1 * x)
 
 
 def visualisation() -> None:
     """Visualise ff."""
     x_range = np.linspace(0.01, 5, 100)
-    x_target = 2
-    x_lbl = "1-residual"
-    y_lbl = "$c$ 1-residual"
+    x_lbl = res_str
+    y_lbl = f"$c$ {res_str}"
 
     fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(8, 5))
 
-    yvalues = scoring_function(x_range, x_target, 10.0)
+    yvalues = scoring_function(
+        x_range,
+        EnvVariables.found_max_r_works,
+        EnvVariables.rbeta,
+    )
 
     ax.plot(x_range, yvalues, c="tab:blue")
 
     ax.tick_params(axis="both", which="major", labelsize=16)
     ax.set_xlabel(x_lbl, fontsize=16)
     ax.set_ylabel(y_lbl, fontsize=16)
-    ax.axvline(x=x_target, c="k")
+    ax.axvline(x=EnvVariables.found_max_r_works, c="k")
 
     fig.tight_layout()
     fig.savefig(
         pathlib.Path("/home/atarzia/workingspace/cpl/figures/cs1/")
-        / "ff_param.png",
+        / "confidence_param.png",
         dpi=360,
         bbox_inches="tight",
     )
