5441d22d22d509bfb9fe2e0f995b7e8fd861d83f
andrewtarzia <andrew.tarzia@gmail.com>
2025-01-24 14:56:47 +0100

    Refactor functions from scripts.

--- src/shc/study_2.py
+++ src/shc/study_2.py
@@ -1,5 +1,13 @@
 """Module for geometry functions."""
 
+from shc._internal.study_2.ligand_utilities import (
+    build_ligand,
+    explore_ligand,
+    normalise_names,
+    passes_dedupe,
+    symmetry_check,
+    update_keys_db,
+)
 from shc._internal.study_2.utilities import (
     FakeMacro,
     analyse_ligand_pair,
@@ -17,13 +25,19 @@ from shc._internal.study_2.utilities import (
 __all__ = [
     "FakeMacro",
     "analyse_ligand_pair",
+    "build_ligand",
+    "explore_ligand",
     "get_pd_polymer",
     "merge_two_ligands",
     "merge_two_ligands_with_cg",
+    "normalise_names",
+    "passes_dedupe",
     "plot_states",
     "plot_timings",
     "reanalyse_ligand_pair",
+    "symmetry_check",
     "to_cg_chemiscope",
     "to_chemiscope",
     "to_csv",
+    "update_keys_db",
 ]
